8e3b6fbf7498ba42f026341b157bdd64444c39e5
alysef <31933995+alysef@users.noreply.github.com>
2017-11-16 21:20:03 -0700

    11/16-10

--- cit230/assignments/lesson-10/json/heroes.js
+++ cit230/assignments/lesson-10/json/heroes.js
@@ -1,3 +1,6 @@
+var header = document.querySelector('header');
+    var section = document.querySelector('section');
+
 var requestURL = 'https://mdn.github.io/learning-area/javascript/oojs/json/superheroes.json';
 
 var request = new XMLHttpRequest();
